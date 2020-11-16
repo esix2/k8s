033a38c68ce02a85708ce8e26fb4d403e210b26c
Ehsan Zandi <e.zandi@gmail.com>
2020-11-16 16:02:24 +0100

    single-container reverted

---
+++ single-container/getRole.py
@@ -0,0 +1,8 @@
+def getRole(person):
+  if person=="ez":
+    return {"name": "Ehsan Zandi", "role" : "new member"}
+  elif person=="jj":
+    return {"name": "Jeniffer Juchens", "role" : "Head of Human Resources"}
+  elif person=="je":
+    return {"name": "Justin Eichenlaub", "role" : "Head of Cellular Solution"}
+  return {"name": 0, "role" : 0}
--- single-container/interview.py
+++ single-container/interview.py
@@ -1,8 +1,8 @@
 import dash
+from flask import current_app
 import dash_html_components as html
 import dash_core_components as dcc
-from flask import Flask
-import requests
+from getRole import getRole
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
@@ -15,7 +15,7 @@ app.layout = html.Div([
                  ]),
     ])),
     #html.H1(['This is simple demo for ', html.Img(style={"padding": 0, "width": "160px", "height": "90px"}, src=app.get_asset_url('logo-m3connect.png')), ' interview']),
-    html.H2('Choose the colleague to get query'),
+    html.H4('Choose the colleague to get query'),
     dcc.Dropdown(
         id='demo-dropdown',
         options=[
@@ -32,26 +32,14 @@ app.layout = html.Div([
 @app.callback(
     dash.dependencies.Output('dd-output-container', 'children'),
     [dash.dependencies.Input('demo-dropdown', 'value')])
-#def dummy_wait_mesage(value):
-#      return [
-#      html.H1(""), html.H1(""), html.H1(""),
-#      html.H3('the query is sent to the database. Please wait!'),
-#      update_output(value)
-#      ]
-
 def update_output(value):
-    t = requests.get(f"http://127.0.0.1:2000/{value}").text
-    idx1 = t.find(':'); idx2 = t.find('"',idx1+1)+1; idx3 = t.find('"',idx2); idx4 = t.find(':',idx3); idx5 = t.find('"',idx4+1)+1; idx6 = t.find('"',idx5);
-    #query = getRole(value)
-    name = t[idx2:idx3]
-    role = t[idx5:idx6]
-    if not name == "role":
-      return [
-      html.H1(""), html.H1(""), html.H1(""),
-      html.H6([html.Span(name,style={"color":"black",'font-weight':'bold'}),' is the ',html.Span(role,style={"color":"black",'font-weight':'bold'})])
-      ]
+    query = getRole(value)
+    name = query.get('name')
+    role = query.get("role")
+    if not name == 0:
+      return html.H1([name,' is the ',role])
       #return [name,' is the ',role]
 
 
 if __name__ == '__main__':
-    app.run_server(debug=True, host="127.0.0.1", port=8080)
+    app.run_server(debug=True, host="0.0.0.0", port=8080)
